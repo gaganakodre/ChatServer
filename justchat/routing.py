a087d6e0dafa89ae3c353b008a6f66c686d82c59
shreegowri <gaganandh@gmail.com>
2022-09-28 10:59:19 +0530

    [shreegowri]created the chat server application with django channels

--- justchat/routing.py
+++ justchat/routing.py
@@ -1,8 +1,10 @@
 # chat/routing.py
+from django.template.defaulttags import url
 from django.urls import path
 
 from . import consumers
 
 websocket_urlpatterns = [
     path('ws/chat/<str:room_name>/', consumers.ChatConsumer.as_asgi()),
+
 ]
--- justchat/urls.py
+++ justchat/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<str:room_name>/', views.room, name='room'),
-    # path('message/', views.create_chat,name='create_chat'),
+
 
 ]
